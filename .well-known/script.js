26c3e0d7160bf29399e3aa9e5407788acaaf22f7
Benjamin Meadows <bemeadows@gmail.com>
2023-10-05 11:05:07 -0700

    Update script.js

--- .well-known/script.js
+++ .well-known/script.js
@@ -3,12 +3,17 @@ async function displaySvgz(url) {
     const response = await fetch(url);
     const compressedData = await response.arrayBuffer();
 
-    // Dynamically import pako as a module
-    const { inflate } = await import('https://ordinals.com/content/fba6f95fb1152db43304a27dce8cb8c65509eba6ab0b6958cedeb33e5f443077i0');
+    // Dynamically load pako by creating a script element
+    const script = document.createElement('script');
+    script.src = 'https://ordinals.com/content/fba6f95fb1152db43304a27dce8cb8c65509eba6ab0b6958cedeb33e5f443077i0';
+    document.head.appendChild(script);
 
-    const decompressedData = inflate(new Uint8Array(compressedData), { to: 'string' });
-    const embedContainer = document.getElementById('svg-container');
-    embedContainer.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(decompressedData)}`;
+    // Wait for the script to load, then use pako
+    script.onload = function () {
+        const decompressedData = pako.inflate(new Uint8Array(compressedData), { to: 'string' });
+        const embedContainer = document.getElementById('svg-container');
+        embedContainer.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(decompressedData)}`;
+    };
 }
 
 // Retrieve the URL parameter from the script tag
