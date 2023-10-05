7014ea75d6e150f2940fbd4622977adf9246bc15
Benjamin Meadows <bemeadows@gmail.com>
2023-10-05 11:01:15 -0700

    Update script.js

--- .well-known/script.js
+++ .well-known/script.js
@@ -1,10 +1,14 @@
 // script.js
-import { inflate } from 'https://ordinals.com/content/fba6f95fb1152db43304a27dce8cb8c65509eba6ab0b6958cedeb33e5f443077i0';
+// Load pako library using a script tag
+const pakoScript = document.createElement('script');
+pakoScript.src = 'https://ordinals.com/content/fba6f95fb1152db43304a27dce8cb8c65509eba6ab0b6958cedeb33e5f443077i0';
+document.head.appendChild(pakoScript);
 
+// Function to display SVGZ
 async function displaySvgz(url) {
     const response = await fetch(url);
     const compressedData = await response.arrayBuffer();
-    const decompressedData = inflate(new Uint8Array(compressedData), {to: 'string'});
+    const decompressedData = pako.inflate(new Uint8Array(compressedData), { to: 'string' });
     const embedContainer = document.getElementById('svg-container');
     embedContainer.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(decompressedData)}`;
 }
@@ -13,4 +17,7 @@ async function displaySvgz(url) {
 const scriptTag = document.currentScript;
 const svgzUrl = scriptTag.getAttribute('data-svgz-url');
 
-displaySvgz(svgzUrl);
+// Wait for pako to be loaded, then display the SVGZ
+pakoScript.onload = function () {
+    displaySvgz(svgzUrl);
+};
