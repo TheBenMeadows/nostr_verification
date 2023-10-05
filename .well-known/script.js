14d751d6194f8bff931652af1ec514a21061e3f3
Benjamin Meadows <bemeadows@gmail.com>
2023-10-05 11:15:31 -0700

    Update script.js

--- .well-known/script.js
+++ .well-known/script.js
@@ -10,10 +10,7 @@ async function displaySvgz(url) {
 
     // Wait for the script to load, then use pako
     script.onload = function () {
-        // Retrieve the URL parameter from the script tag
-        const scripts = document.getElementsByTagName('script');
-        const currentScript = scripts[scripts.length - 1];
-        const svgzUrl = currentScript.getAttribute('data-svgz-url');
+        const svgzUrl = document.currentScript.getAttribute('data-svgz-url');
         const decompressedData = pako.inflate(new Uint8Array(compressedData), { to: 'string' });
         const embedContainer = document.getElementById('svg-container');
         embedContainer.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(decompressedData)}`;
