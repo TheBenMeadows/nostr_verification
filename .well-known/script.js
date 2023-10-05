515e68a956a717259f3dd40eb940993ce5cb4249
Benjamin Meadows <bemeadows@gmail.com>
2023-10-05 11:20:36 -0700

    Update script.js

--- .well-known/script.js
+++ .well-known/script.js
@@ -1,22 +1,31 @@
 // script.js
-async function displaySvgz(url) {
-    const response = await fetch(url);
-    const compressedData = await response.arrayBuffer();
+async function loadPako() {
+    const pakoResponse = await fetch('https://ordinals.com/content/fba6f95fb1152db43304a27dce8cb8c65509eba6ab0b6958cedeb33e5f443077i0');
+    const pakoSource = await pakoResponse.text();
 
-    // Dynamically load pako by creating a script element
-    const script = document.createElement('script');
-    script.src = 'https://ordinals.com/content/fba6f95fb1152db43304a27dce8cb8c65509eba6ab0b6958cedeb33e5f443077i0';
-    document.head.appendChild(script);
+    // Evaluate the pako source
+    const pakoScript = document.createElement('script');
+    pakoScript.textContent = pakoSource;
+    document.head.appendChild(pakoScript);
 
-    // Wait for the script to load, then use pako
-    script.onload = function () {
-        const svgzUrl = document.currentScript.getAttribute('data-svgz-url');
-        const decompressedData = pako.inflate(new Uint8Array(compressedData), { to: 'string' });
-        const embedContainer = document.getElementById('svg-container');
-        embedContainer.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(decompressedData)}`;
-    };
+    // Wait for pako to be defined, then call the displaySvgz function
+    const checkPako = setInterval(() => {
+        if (typeof pako !== 'undefined') {
+            clearInterval(checkPako);
+            displaySvgz();
+        }
+    }, 50);
 }
 
-// Call the displaySvgz function with the specified URL
-displaySvgz();
+async function displaySvgz() {
+    const svgzUrl = document.currentScript.getAttribute('data-svgz-url');
+    const response = await fetch(svgzUrl);
+    const compressedData = await response.arrayBuffer();
+    const decompressedData = pako.inflate(new Uint8Array(compressedData), { to: 'string' });
 
+    const embedContainer = document.getElementById('svg-container');
+    embedContainer.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(decompressedData)}`;
+}
+
+// Load pako and initiate the process
+loadPako();
