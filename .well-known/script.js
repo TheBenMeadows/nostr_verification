9836bca2217fde2c69ba3d1b8ad4c5d03b2e7177
Benjamin Meadows <bemeadows@gmail.com>
2023-10-05 10:53:15 -0700

    Add files via upload

---
+++ .well-known/script.js
@@ -0,0 +1,16 @@
+// script.js
+import { inflate } from 'data:application/javascript;base64,${btoa(await(await fetch('https://ordinals.com/content/fba6f95fb1152db43304a27dce8cb8c65509eba6ab0b6958cedeb33e5f443077i0')).text())}';
+
+async function displaySvgz(url) {
+    const response = await fetch(url);
+    const compressedData = await response.arrayBuffer();
+    const decompressedData = inflate(new Uint8Array(compressedData), {to: 'string'});
+    const embedContainer = document.getElementById('svg-container');
+    embedContainer.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(decompressedData)}`;
+}
+
+// Retrieve the URL parameter from the script tag
+const scriptTag = document.currentScript;
+const svgzUrl = scriptTag.getAttribute('data-svgz-url');
+
+displaySvgz(svgzUrl);
