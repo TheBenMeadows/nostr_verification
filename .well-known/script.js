2ece8e6a40c1a2c890e673de7ce04db4bee6381a
Benjamin Meadows <bemeadows@gmail.com>
2023-10-05 12:14:48 -0700

    Update script.js

--- .well-known/script.js
+++ .well-known/script.js
@@ -1,24 +1,18 @@
-console.log("Traditional script is running.");
+console.log("Script is starting...");
 
 async function displaySvgz(url) {
-    console.log("Fetching SVGZ from:", url);
-    
+    console.log("Attempting to fetch:", url);
+
     const response = await fetch(url);
     const compressedData = await response.arrayBuffer();
     
-    console.log("Decompressing SVGZ...");
-    const decompressedData = pako.inflate(new Uint8Array(compressedData), {to: 'string'});
+    console.log("Attempting to decompress...");
+
+    const decompressedData = pako.inflate(new Uint8Array(compressedData), { to: 'string' });
     console.log("Decompressed SVG first 100 chars:", decompressedData.substring(0, 100));
-    
-    // Parse the SVG data and insert it directly into the body
-    console.log("Parsing and inserting SVG...");
-    const parser = new DOMParser();
-    const svgDoc = parser.parseFromString(decompressedData, "image/svg+xml");
-    const svgElement = svgDoc.documentElement;
-    
-    // Append the SVG element to the body
-    document.body.appendChild(svgElement);
-    console.log("SVG should be displayed now.");
+
+    const embedContainer = document.getElementById('svg-container');
+    embedContainer.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(decompressedData)}`;
 }
 
 const svgzUrl = 'https://raw.githubusercontent.com/TheBenMeadows/HostedFiles/master/Mask%20-%20Bulls%20Zip.svgz';
